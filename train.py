8b6ea90199cbd6b8df969cacf848e1ac9d1ba391
ahma2860 <149191001+rimanoori@users.noreply.github.com>
2025-08-11 20:04:28 -0400

    3rd commit

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ y = df[df.columns[2]]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # === Step 3: Train model ===
-model = LinearRegression(max_iter=500)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 # === Step 4: Evaluate model ===
